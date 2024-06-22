b34524a4e46ef999c67d79f315cbf1f63b0f547a
ileanahi <argyris.ileana@gmail.com>
2024-06-22 00:26:59 -1000

    Create required functions

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+import getRandomWord from './src/randomWord';
+import setSharkImage from './src/sharkImage';
+import setupGuesses from './src/guess.js';
+import { setupWord, isLetterInWord, revealLetterInWord } from './src/word.js';
+
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
@@ -12,10 +17,52 @@ document.querySelector('#app').innerHTML = `
 
 const initSharkwords = () => {
   let numWrong = 0;
-  const word = 'word';
+  const sharkImg = document.querySelector('#shark-img');
+  const status = document.querySelector('#game-status');
+
+  const word = getRandomWord();
+  setSharkImage(sharkImg, numWrong);
+  setupWord(document.querySelector('#word-container'), word);
+  let isComplete = true;
+  const handleGuess = (guessEvent, letter) => {
+    const btn = guessEvent.target;
+    const letterboxes = document.querySelectorAll('.letter-box')
+    btn.disabled = true;
+    if(isLetterInWord(letter)) {
+      revealLetterInWord(letter);
+    } else {
+      numWrong++;
+      setSharkImage(sharkImg, numWrong); 
+    }
+    
+    // tests if every letterbox is filled, returns false if one value is empty
+    isComplete = Array.from(letterboxes).every(
+      (el) => el.innerText !== '',
+    );
+    if(isComplete || numWrong == 5) {
+      const btns = document.querySelectorAll('button');
+      btns.forEach((ele) => {
+        ele.disabled = true;
+      });
+    }
+    
+    if(numWrong == 5) {
+      // End Game
+      status.innerText = "You lose!";
+    }
+
+    if(isComplete) {
+      status.innerText = "You win!";
+    }
+  };
+  
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess);
 
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
+  console.log(isLetterInWord(word[0]), '(should be true)');
+  console.log(isLetterInWord('1'), '(should be false)');
+
 };
 
 initSharkwords();
--- src/guess.js
+++ src/guess.js
@@ -1,10 +1,14 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 function setupGuesses(element, handleGuess) {
+  // for all letters in alphabet
   alphabet.split('').forEach((letter) => {
+    // create a button with each letter in the alphabet
     const btn = document.createElement('button');
     btn.innerText = letter;
+    // attach handleGuess event to click event of each button
     btn.addEventListener('click', (e) => handleGuess(e, letter));
+    // add button to element
     element.append(btn);
   });
 }
